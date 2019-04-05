0e3cec5195029839b15211d2f084cf57cf06f745
tl17 <tuanlinhtl17@gmail.com>
2019-04-05 11:54:33 +0000

    Updates

---
+++ precache-manifest.f21ef5faaf2b13890b941f3b37e3b87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "b6e5233cc5fd76ce2894",
+    "url": "/robofriends/static/js/main.b6e5233c.chunk.js"
+  },
+  {
+    "revision": "cfc387841a7e6aa79f1a",
+    "url": "/robofriends/static/js/2.cfc38784.chunk.js"
+  },
+  {
+    "revision": "b6e5233cc5fd76ce2894",
+    "url": "/robofriends/static/css/main.a0e89fc1.chunk.css"
+  },
+  {
+    "revision": "cfc387841a7e6aa79f1a",
+    "url": "/robofriends/static/css/2.42298f06.chunk.css"
+  },
+  {
+    "revision": "9b6aff8ba9dc2c4e11cd5de05b820daf",
+    "url": "/robofriends/index.html"
+  }
+];
